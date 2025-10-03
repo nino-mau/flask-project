cb6f0f6d15aecd22aa47ee77716d5d6a0fb59c2c
nino <nino.mauuary@gmail.com>
2025-10-03 15:19:21 +0200

    [feat] change image dimensions requirements

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -56,7 +56,7 @@ import type { FormSubmitEvent } from '@nuxt/ui';
 import * as z from 'zod';
 
 const MAX_FILE_SIZE = 2 * 1024 * 1024; // 2MB
-const MIN_DIMENSIONS = { width: 200, height: 200 };
+const MIN_DIMENSIONS = { width: 50, height: 50 };
 const MAX_DIMENSIONS = { width: 4096, height: 4096 };
 const ACCEPTED_IMAGE_TYPES = [
   'image/jpeg',
@@ -118,10 +118,12 @@ const state = reactive<Partial<schema>>({
 });
 
 async function onSubmit(event: FormSubmitEvent<schema>) {
+  /**
+   * Upload image
+   */
   const formData = new FormData();
   formData.append('file', event.data.image);
-  console.log(formData);
-  const { data } = await $fetch('/api/upload', {
+  const data = await $fetch.raw('/api/character', {
     method: 'POST',
     body: formData
   });
