e4e905b367b776cae3dc7b8f19b06a54ff04d474
nino <nino.mauuary@gmail.com>
2025-10-07 14:34:39 +0200

    [feat] edit nuxt ui theme

---
+++ frontend/app.config.ts
@@ -0,0 +1,25 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      primary: "blue",
+      secondary: "pink",
+      tertiary: "orange",
+      info: "sky",
+      warning: "orange",
+      error: "red",
+      accent: "violet",
+    },
+    theme: {
+      colors: [
+        "primary",
+        "secondary",
+        "tertiary",
+        "info",
+        "success",
+        "warning",
+        "error",
+        "accent",
+      ],
+    },
+  },
+});
