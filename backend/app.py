a472f3abd397d62ff875223b6bdbe435fda21a5b
nino <nino.mauuary@gmail.com>
2025-10-21 14:49:22 +0200

    [feat] switch to a google cloudsql postgres database

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,5 @@
 import os
 
-import psycopg2
 from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
@@ -8,25 +7,21 @@ from model import db
 from routes.character import character_bp
 from routes.characters import characters_bp
 from routes.character.export import export_bp
+from cloudsql_connect import get_engine
 
 load_dotenv()
 
 app = Flask(__name__)
 
-# Configure app
-
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+engine = get_engine()
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+pg8000://"
+app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"creator": engine.pool._creator}
 db.init_app(app)
 
 CORS(app)
 
 with app.app_context():
     db.create_all()
-
-
-def get_db_connection():
-    conn = psycopg2.connect(os.environ["DATABASE_URL"])
-    return conn
 
 
 # Register routes
